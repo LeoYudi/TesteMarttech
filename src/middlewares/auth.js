100e2f54955a64f850e666d9690cb65b363459c2
Leonardo Yudi Higuti <leonardo.higuti@unesp.br>
2020-12-15 11:46:08 -0300

    add middleware to verify token

---
+++ src/middlewares/auth.js
@@ -0,0 +1,27 @@
+const jwt = require('jsonwebtoken');
+const config = require('../config/token');
+
+module.exports = (req, res, next) => {
+  const authHeader = req.headers.authorization;
+
+  if (!authHeader)
+    return res.status(401).json({ msg: 'No token provided' });
+
+  const parts = authHeader.split(' ');
+  if (!parts.length == 2)
+    return res.status(401).json({ msg: 'Token error' });
+
+  const [scheme, token] = parts;
+
+  if (!/^Bearer$/i.test(scheme))
+    return res.status(401).json({ msg: 'Token malformatted' })
+
+  jwt.verify(token, config.secret, (err, decoded) => {
+    if (err)
+      return res.status(401).json({ msg: 'Token invalid' });
+
+    req.id = decoded.id;
+    req.level = decoded.level;
+    return next();
+  })
+};
